5baff8429c7ba328e20178d6e7642158a7588d3a
KD01-1494 <alexserg20sergeev@yandex.ru>
2022-01-26 13:44:14 +0600

    d

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
-browser = webdriver.Chrome()
+DRIVER_PATH = ''
+
+options = Options()
+options.add_argument('--headless')
+options.add_argument('--disable-gpu')
+
+browser = webdriver.Chrome(chrome_options=options)
 browser.get('https://google.com')
 
 print(browser.title)
