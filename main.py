fc7c1d2966f5ed96c43cc1015313c9aea1c0c159
Alexey <91065165+KD01-1494@users.noreply.github.com>
2022-01-31 21:56:21 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from selenium import webdriver
 import os
 
+screen_path = './full.png'
+
 chrome_options = webdriver.ChromeOptions()
 chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
 chrome_options.add_argument("--headless")
@@ -10,4 +12,6 @@ chrome_options.add_argument("--no-sandbox")
 driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
 
 driver.get('https://google.com')
+driver.save_screenshot(screen_path)
+
 print(driver.title)
